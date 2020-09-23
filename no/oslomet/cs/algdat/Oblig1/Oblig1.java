3a267da586e2670e37e32f49c427cce420f340d1
mohamedabdi <m.abdillahi1205@hotmail.com>
2020-09-23 15:32:05 +0200

    Initial commit

---
+++ no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -0,0 +1,189 @@
+package no.oslomet.cs.algdat.Oblig1;
+
+////// Løsningsforslag Oblig 1 ////////////////////////
+
+import java.lang.UnsupportedOperationException;
+
+public class Oblig1 {
+    private Oblig1() {}
+    private static void bytt (int [] a, int i, int j){
+        int temp = a[i];
+        a[i]= a[j];
+        a[j]= temp;
+    }
+    private static void bytt (char []a , int i, int j){
+        char temp = a[i];
+        a[i]= a[j];
+        a[j]= temp;
+    }
+
+    ///// Oppgave 1 //////////////////////////////////////
+    /* Det er flest ombyttinger når array ikke er sortert
+        Det er færrest når array er sortert stignende.
+        I gjennomsnitt er det H(n) -1 ombyttinger
+
+     */
+    public static int maks(int[] a) {
+        throw new UnsupportedOperationException("Tabellen er tom!");
+
+        int j = 0;
+
+        for (int i = 1; i < a.length; ++i) {
+            if (a[i] > a[j]) j = i;
+        }
+        return j;
+    }
+
+    public static int ombyttinger(int[] a) {
+        throw new UnsupportedOperationException();
+        if (a.length<1){throw new UnsupportedOperationException();}
+
+        int antallOmbyttinger= 0;
+
+        for (int i=1;i<a.length;++i){
+            if (a[i+1]>a[i]){
+                bytt(a, i+1, i);
+                antallOmbyttinger++;
+            }
+        }
+        return antallOmbyttinger;
+    }
+
+    ///// Oppgave 2 //////////////////////////////////////
+    public static int antallUlikeSortert(int[] a) {
+        throw new UnsupportedOperationException();
+        {
+            if (antallUlikeSortert(1))
+                return 1;
+        }
+        int antallSortert= 0;
+        for (int i=1; i<a.length; ++i){
+            if (a[i-1]>a[i]){
+                bytt(a, i-1, i);
+                antallSortert++;
+            }
+        }
+        return antallSortert;
+    }
+
+    ///// Oppgave 3 //////////////////////////////////////
+    public static int antallUlikeUsortert(int[] a) {
+        throw new UnsupportedOperationException();
+        {
+            if (a.length<2)
+                return a.length;
+        }
+        int antallUlike= 1;
+
+        for (int i=1;i<a.length;++i){
+            int j=0;
+            for (int j=0; j<i;++j){
+                int verdi=a[i];
+                if (a[j]==verdi) break;
+            }
+            if (j==i) antallUlike++;
+        }
+        return antallUlike;
+    }
+
+    ///// Oppgave 4 //////////////////////////////////////
+    public static void delsortering(int[] a) {
+        throw new UnsupportedOperationException();
+
+        int left =0,  right=a.length - 1;
+        while (left<right){
+            while (a[left]%2 == 0 && left<right)
+                left++;
+            while (a[right]% 2 ==1 && left>right)
+                right--;
+            if (left<right){
+                int temp = a[left];
+                a[left]= a[right];
+                a[right]= temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    ///// Oppgave 5 //////////////////////////////////////
+    public static void rotasjon(char[] a) {
+        throw new UnsupportedOperationException();
+        if (a.length> 1){
+            char temp= a[a.length-1];
+
+            for (int i=a.length-1;i>0; --1){
+                a[i]= a[i-1];
+            }
+            a[0]= temp;
+        }
+    }
+
+    ///// Oppgave 6 //////////////////////////////////////
+    public static void rotasjon(char[] a, int k) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 7 //////////////////////////////////////
+    /// 7a)
+    public static String flett(String s, String t) {
+        //throw new UnsupportedOperationException();
+        char [] c= s.toCharArray();
+        char [] d= t.toCharArray();
+        char [] flett= new char [c.length + d.length];
+
+        int k= Math.min(c.length, d.length);
+
+        for (int i=0, j=0; i<k; ++i){
+            flett[j++]= c[i];
+            flett[j++]= d[i];
+        }
+
+        System.arraycopy(c, k, flett, 2*k, c.length - k);
+        System.arraycopy(d, k, flett, 2*k, c.length - k);
+        return String.valueOf(flett);
+    }
+
+    /// 7b)
+    public static String flett(String... s) {
+        throw new UnsupportedOperationException();
+        if (s.length==0)
+            return "";
+        int k= s[0].length();
+
+        for (int i= 1; i<s.length; ++i){
+            int stringlength= s[i].length();
+            if (stringlength> k)
+                k=stringlength;
+        }
+
+        StringBuilder stringBuilder= new StringBuilder ();
+
+        for (int i= 0; i<k; i++){
+            for (String t:s){
+                if (i<t.length()) stringBuilder.append(t.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    ///// Oppgave 8 //////////////////////////////////////
+    public static int[] indekssortering(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 9 //////////////////////////////////////
+    public static int[] tredjeMin(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 10 //////////////////////////////////////
+    public static int bokstavNr(char bokstav) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean inneholdt(String a, String b) {
+        throw new UnsupportedOperationException();
+    }
+
+}  // Oblig1
